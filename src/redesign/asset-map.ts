8268b48ea356db7aa808649bb54ba0227de54227
dasomel <dasomell@gmail.com>
2026-08-25 13:18:47 +0900

    design: catalog existing OPDC assets for redesign

---
+++ src/redesign/asset-map.ts
@@ -0,0 +1,14 @@
+export const REDESIGN_ASSETS = {
+  organization: '/img/about/organization.webp',
+  organizationEn: '/img/about/organization-en.jpg',
+  intro: '/img/about/opdc_img.webp',
+  history: '/img/history/history.webp',
+  leaders: [
+    '/img/leader/01-go-Jaedo.webp',
+    '/img/leader/02-kim-kyungha.webp',
+    '/img/leader/03-lee-seungryong.webp',
+    '/img/leader/04-han-seonggon.webp',
+    '/img/leader/05-lee-kiha.webp',
+    '/img/leader/06-jeong-hoyeol.webp',
+  ],
+} as const
